d1672071ef849f2952fb979f364e7861f0b3a4a8
FlorenciaRossi <flrossi@estudiantes.unsam.edu.ar>
2024-02-09 22:01:18 -0300

    new feature math

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Math calculo = new Math();
+        System.out.println( calculo.suma(4, 6));
+        System.out.println( calculo.resta(4, 6));
+        System.out.println( calculo.division(4, 6));
+        System.out.println( calculo.multiplicacion(4, 6));
+
     }
 }
---
+++ src/Math.java
@@ -0,0 +1,6 @@
+public class Math {
+    public int suma(int a, int b){ return a + b; }
+    public int resta(int a, int b){ return a - b; }
+    public int multiplicacion(int a, int b){ return a * b; }
+    public float division(float a, float b ){ return a/b; }
+}
